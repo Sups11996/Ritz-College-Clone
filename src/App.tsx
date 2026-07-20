c5df52758af6a539d67d6727efe48e1c8a55d3f4
Suprim Khadka <sups11996@gmail.com>
2026-07-20 22:34:41 +0545

    docs: Add JSDoc documentation to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,16 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 
+/**
+ * App Component
+ * 
+ * Root layout component that provides:
+ * - Persistent navigation bar across all routes
+ * - Scroll-to-top functionality on route changes
+ * - Main content area with outlet for nested routes
+ * - Footer section
+ * - Maximum width constraint for better readability
+ */
 function App() {
   return (
     <>
@@ -12,9 +22,6 @@ function App() {
       <main className="min-h-[calc(100vh-100px)] max-w-384 mx-auto">
         <Outlet />
       </main>
-
-        {/* <Outlet /> */}
-
 
       <Footer />
     </>
